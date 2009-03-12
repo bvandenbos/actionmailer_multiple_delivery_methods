b2fc9f10f90520bfda8a6118811fb5e929ce55e4
Ben VandenBos <ben@ben-01.local>
2009-03-12 14:45:31 -0700

    change

--- lib/actionmailer_multiple_delivery_methods.rb
+++ lib/actionmailer_multiple_delivery_methods.rb
@@ -1,7 +1,7 @@
-module ActionMailer
+class ActionMailer::Base
   
   class << self
-  
+
     # override deliver! to support mutliple methods
     def deliver!(mail = @mail)
       raise "no mail object available for delivery!" unless mail
@@ -12,6 +12,7 @@ module ActionMailer
 
       begin
         Array(delivery_method).each do |dm|
+          puts "delivering #{dm}"
           __send__("perform_delivery_#{dm}", mail) if perform_deliveries
         end
       rescue Exception => e  # Net::SMTP errors or sendmail pipe errors
@@ -20,7 +21,7 @@ module ActionMailer
 
       return mail
     end
-    
+
   end
   
 end
